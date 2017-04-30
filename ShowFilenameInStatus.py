0cfb251346de7133739315ca08fe54d296e777c6
demastrie <michael@demastrie.com>
2017-04-30 11:18:39 -0400

    Update ShowFilenameInStatus.py
    Changed the behavior to show the full path of the file, truncated to the  last MAX_FILE_LENGTH characters.

--- ShowFilenameInStatus.py
+++ ShowFilenameInStatus.py
@@ -1,9 +1,13 @@
-import sublime, sublime_plugin, os
+import sublime_plugin
+
+MAX_FILE_LENGTH = 100
+
 
 class ShowFilenameInStatus(sublime_plugin.EventListener):
-	def on_activated_async(self, view):
-	    filename = os.path.split(view.file_name())[1]
-	    if filename is None:
-	        view.erase_status('_filename')
-	    else:
-	        view.set_status('_filename', "File: " + filename)
+    def on_activated_async(self, view):
+        filename = view.file_name()
+        view.erase_status('_filename')
+        if filename is not None:
+            if len(filename) > MAX_FILE_LENGTH:
+                filename = filename[-(MAX_FILE_LENGTH - 3):]
+            view.set_status('_filename', 'File "...{}"'.format(filename))
